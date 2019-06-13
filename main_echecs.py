f3177eab5bf55c646ca79eec3f6cc40d2162d5aa
kpoissonnier <k.poissonnier@gmail.com>
2019-06-13 16:59:52 +0200

    mise a jour de main

--- main_echecs.py
+++ main_echecs.py
@@ -34,9 +34,13 @@ on affichera toutes les pieces sur les nouvelles positions coté utilisateur.
 -systeme de sauvegarde de partie coté serveur
 -a vous de trouver
 """
+
 liste_pieces=[]
 liste_positions=[]
-
+liste_positions_noire=[]
+liste_positions_blanche=[]
+liste_colonnes=("A","B","C","D","E","F","G","H")
+liste_lignes=(1,2,3,4,5,6,7,8)
 class piece():
     def __init__ (self, couleur, typePiece, position):
         self.couleur = couleur #blanc ou noir
@@ -44,14 +48,58 @@ class piece():
         self.position= position # coordonnées
         self.enjeu=True #enjeu ou pas (pris)
         self.moved=False # a bougé (True) ou non (False)
+        self.controle=[] #liste des cases sur laquelle la pièce peut se déplacer ou prendre une pièce
     def mouvement(arrivee):
         self.position=arrivee 
         self.moved=True #on change l'état du moved pour marquer que la pièce a bougé (pour le roc)
-
-#J'ai besoin que tous les objets pièces soient dans une liste liste_pieces
+    def casescontrolees():
+        pass
+    
+# J'ai besoin que tous les objets pièces soient dans une liste liste_pieces
 # pour pouvoir faire une liste des positions liste_positions
-#pour ensuite tester ces positions dans les mouvements
+# pour ensuite tester ces positions dans les mouvements
+
 
 for i in liste_pieces:
     liste_positions.append(i.position)
+    if i.couleur=="blanc":
+        liste_positions_blanche.append(i.position)
+    else:
+        liste_positions_noire.append(i.position)
 
+
+#essai de programmation des mouvements
+
+def casescontrolees():
+    if self.typePiece=="pion":
+        #le pion ne peux qu'avancer tout droit
+        #ou prendre en biais
+        #en fonction de sa couleur : vers le haut pour les blancs, vers le bas pour les noirs
+        if self.couleur=="blanc":
+            colonne=self.position[0]
+            ligne=self.position[1]
+            #case devant
+            if (colonne,ligne+1) not in liste_positions:
+                self.casescontrolees.append((colonne,ligne+1))
+            #case avant gauche
+            if colonne!="A" and 
+            #case avant droite
+
+
+
+"""
+Je vais créer des modules pour les mouvements de base (les 8 directions cardinales)
+pour faire des tests dans les mouvements des différentes pièces
+En gros : 
+1. on regarde la couleur de la pièce (self.couleur)
+2. dans les directions du déplacement de la pièce, on regarde si la case est libre 
+ou occupée par une pièce adverse (d'une autre couleur voir 
+liste_positions_noire et liste_positions_blanc)
+Alors on enregistre la case dans les lignes controlées
+3. On continue dans les directions
+4. si on atteint un bord ou une pièce de notre couleur, on arrête
+5. la liste des cases controlées permet de savoir si les déplacements demandés sont légaux ou pas.
+de voir si le roi est en échec
+si le roi a le droit de bouger dans une case...
+
+"""
