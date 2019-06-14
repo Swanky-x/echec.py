abcd668444523f74008670dbcaa97ec5f7726819
kpoissonnier <k.poissonnier@gmail.com>
2019-06-14 16:39:21 +0200

    le main du vendredi fin d'après midi, avec les différentes fonctions de check de mouvement. Pour la suite je dois taper dans la bdd

--- main_echecs.py
+++ main_echecs.py
@@ -36,35 +36,35 @@ on affichera toutes les pieces sur les nouvelles positions coté utilisateur.
 """
 
 
-liste_pieces=[]
-liste_positions=[]
-liste_positions_noire=[]
-liste_positions_blanche=[]
-liste_colonnes=("A","B","C","D","E","F","G","H")
-liste_lignes=(1,2,3,4,5,6,7,8)
+# liste_pieces=[]
+# liste_positions=[]
+# liste_positions_noire=[]
+# liste_positions_blanche=[]
+# liste_colonnes=("A","B","C","D","E","F","G","H")
+# liste_lignes=(1,2,3,4,5,6,7,8)
 
-cadre=[]
-for i in 1,2,3,4,5,6,7,8:
-    for j in 1,2,3,4,5,6,7,8:
-        cadre.append(int(f"{i}{j}"))
+# cadre=[]
+# for i in 1,2,3,4,5,6,7,8:
+#     for j in 1,2,3,4,5,6,7,8:
+#         cadre.append(int(f"{i}{j}"))
 
-print(cadre)
+# print(cadre)
 
-def surLePlateau(case):
-    if case in cadre:
-        print(case,"est sur le plateau")
-        return True
-    else:
-        print(case, "n'est pas sur le plateau")
+# def surLePlateau(case):
+#     if case in cadre:
+#         print(case,"est sur le plateau")
+#         return True
+#     else:
+#         print(case, "n'est pas sur le plateau")
 
-surLePlateau(88)
-surLePlateau(59)
+# surLePlateau(88)
+# surLePlateau(59)
 
 
 
 class piece():
-    def __init__ (self,position):
-        self.couleur = 'requete sql' #blanc ou noir
+    def __init__ (self, couleur, typePiece, position):
+        self.couleur = couleur #'requete sql' #blanc ou noir
         self.typePiece= typePiece  # king queen fou cavalier tour pion
         self.position= position # coordonnées
         self.enjeu=True #enjeu ou pas (pris)
@@ -83,7 +83,7 @@ class piece():
 # pour pouvoir faire une liste des positions liste_positions
 # pour ensuite tester ces positions dans les mouvements
 
-# v=piece('blanc','pion',11)
+
 # w=piece('noir','pion',56)
 # liste_pieces.append(v)
 # liste_pieces.append(w)
@@ -154,41 +154,44 @@ class piece():
 # valeur de départ
 # valeur d'arrivée
 
-caseDepart=0
-caseArrivee=0
+#v=piece('blanc','pion',11)
 
-active=piece(caseDepart)
 
+#mvt du pion
 def mvtPion(pieceActive):
     patern=[]
-    if pieceActive.couleur='blanc':
+    if pieceActive.couleur=='blanc':
         patern.append(pieceActive.position+1)
         patern.append(pieceActive.position+11)
         patern.append(pieceActive.position+9)
         if pieceActive.moved==False:
-            patern.append(pieceActive+2)
-    if pieceActive.couleur='noir':
+            patern.append(pieceActive.position+2)
+    if pieceActive.couleur=='noir':
         patern.append(pieceActive.position-1)
         patern.append(pieceActive.position-11)
         patern.append(pieceActive.position-9)
         if pieceActive.moved==False:
             patern.append(pieceActive-2)
+    patern.sort()
     return patern
 
+#mvt de la tour
 def mvtTour(pieceActive):
     patern=[]
     for i in -7,-6,-5,-4,-3,-2,-1,1,2,3,4,5,6,7:
         patern.append(pieceActive.position+10*i)
         patern.append(pieceActive.position+1*i)
+    patern.sort()
     return patern
-
+#mvt du fou
 def mvtFou(pieceActive):
     patern=[]
     for i in -7,-6,-5,-4,-3,-2,-1,1,2,3,4,5,6,7:
         patern.append(pieceActive.position+11*i)
         patern.append(pieceActive.position+9*i)
+    patern.sort()
     return patern
-
+#mvt de la reine
 def mvtQueen(pieceActive):
     patern=[]
     for i in -7,-6,-5,-4,-3,-2,-1,1,2,3,4,5,6,7:
@@ -196,14 +199,16 @@ def mvtQueen(pieceActive):
         patern.append(pieceActive.position+9*i)
         patern.append(pieceActive.position+10*i)
         patern.append(pieceActive.position+1*i)
+    patern.sort()
     return patern
-
+#mvt du cavalier
 def mvtCavalier(pieceActive):
     patern=[]
     for i in -21,-19,-12,-8,8,12,19,21:
         patern.append(pieceActive.position+i)
+    patern.sort()
     return patern
-
+#mvt du roi
 def mvtKing(pieceActive):
     patern=[]
     for i in -1,1:
@@ -211,10 +216,109 @@ def mvtKing(pieceActive):
         patern.append(pieceActive.position+9*i)
         patern.append(pieceActive.position+10*i)
         patern.append(pieceActive.position+1*i)
+    patern.sort()
     return patern
 
+#la fonction mvt calcule les trajectoires de la pièce
 def mvt(pieceActive):
-    
+    if active.typePiece=='pion':
+        # print (mvtPion(pieceActive))
+        return mvtPion(pieceActive)
+    if active.typePiece=='tour':
+        # print (mvtTour(pieceActive))
+        return mvtTour(pieceActive)
+    if active.typePiece=='fou':
+        # print (mvtFou(pieceActive))
+        return mvtFou(pieceActive)
+    if active.typePiece=='queen':
+        # print (mvtQueen(pieceActive))
+        return mvtQueen(pieceActive)
+    if active.typePiece=='king':
+        #print (mvtKing(pieceActive))
+        return mvtKing(pieceActive)
+    if active.typePiece=='cavalier':
+        #print (mvtCavalier(pieceActive))
+        return mvtCavalier(pieceActive)
+
+#check mouvement vérifie que la caseArrivee est sur les trajectoire de la pièce
+def checkmvt(pieceActive, caseArrivee):
+    if caseArrivee in mvt(pieceActive):
+        print ("mouvement correct")
+        return True
+    else:
+        print("mouvement interdit")
+        return False
+
+
+#la fonction chemin donne le chemin le plus direct pour aller à la destination
+def leChemin(pieceActive, caseArrivee):
+    if not checkmvt(pieceActive,caseArrivee):
+        return False
+    if pieceActive.typePiece=='king' or pieceActive.typePiece=='cavalier':
+        print("pas d'obstacle pour le roi ou le cavalier")
+        chemin=[]
+        return chemin
+    difference=abs(pieceActive.position-caseArrivee)
+    print(difference)
+    for i in 11,10,9:
+        if difference%i==0:
+            quotient=i
+            print (quotient)
+    if difference%11==0 or difference%10==0 or difference%9==0:
+        pass
+    else:
+        quotient=1
+    #     else:
+    #         quotient=1
+    #         print(quotient)
+    # droite=[]
+    # for i in mvt(pieceActive):
+    #     if i%quotient==0:
+    #         droite.append(i)
+    chemin=[]
+    # if pieceActive.position < caseArrivee:
+    #     for i in droite:
+    #         if i > pieceActive.position:
+    #             chemin.append(i)
+    #     chemin.sort()
+    # else:
+    #     for i in droite:
+    #         if i<pieceActive.position:
+    #             chemin.append(i)
+    #     chemin.sort(reverse=True)
+    if pieceActive.position < caseArrivee:
+        temp=0
+        i=1
+        while temp!=caseArrivee:
+            temp=pieceActive.position+i*quotient
+            chemin.append(temp)
+            i+=1
+            print(temp)
+        chemin.sort()
+    if pieceActive.position > caseArrivee:
+        temp=0
+        i=1
+        while temp!=caseArrivee:
+            temp=pieceActive.position-i*quotient
+            chemin.append(temp)
+            i+=1
+            print(temp)
+        chemin.sort(reverse=True)
+    #on sort la desitination de la liste
+    chemin.pop()
+    print(chemin)
+    return chemin
+
+#la maintenant on va taper dans la bdd
+# pour vérifier les positions du chemin et vérifier qu'il n'y a rien sur le chemin
+#def verifObstacle(pieceActive, caseArrivee):
+
+
+
+active=piece('blanc','fou',22)
+# mvt(active)
+# checkmvt(active, 55)
+leChemin(active,31)
 
 
 """
